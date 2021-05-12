7729b31ecddbb62435b44c4a276dd7a205058da3
aanniiee1209 <donganh.vo@web.de>
2021-05-12 17:37:39 +0200

    Create Info.lua

---
+++ Dummy plugin/dummyplugin.lrdevplugin/Info.lua
@@ -0,0 +1,17 @@
+return {
+LrSdkVersion = 9.0,
+LrToolkitIdentifier = "com.adobe.lightroom.sdk.dummyplugin",
+LrPluginName = "Dummy Plugin",
+
+
+LrExportMenuItems = {
+title = "Dummy Plugin Exp", -- The display text for the menu item
+file = "ExportMenuItem.lua", -- The script that runs when the item is selected
+},
+
+LrLibraryMenuItems = {
+title = "Dummy Plugin Lib", -- The display text for the menu item
+file = "LibraryMenuItem.lua", -- The script that runs when the item is selected
+},
+
+}
