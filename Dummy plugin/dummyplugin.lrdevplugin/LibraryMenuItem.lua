3cd48961768367bc2e5fbfdc8d067d35061a4686
ReginaBarsukov <regina_bars@outlook.de>
2021-05-12 19:23:54 +0200

    Added Sliders
    Added a few Slider for example for Light, Color and more Lightroom settings

--- Dummy plugin/dummyplugin.lrdevplugin/LibraryMenuItem.lua
+++ Dummy plugin/dummyplugin.lrdevplugin/LibraryMenuItem.lua
@@ -13,30 +13,90 @@ LrFunctionContext.callWithContext( "showCustomDialog", function( context )
     props.isChecked = false -- add a property key and initial value
     -- create view hierarchy
     local f = LrView.osFactory()
-    local c = f:row { -- the root node
-        bind_to_object = props, -- bound to our data table
-        -- add controls
-        f:checkbox {
-            title = "Enable", -- label text
-            value = LrView.bind( "isChecked" ) -- bind button state to data key
+    local contents = f:column { -- create view hierarchy
+        spacing = f:control_spacing(),
+        bind_to_object = properties, -- default bound table is the one we made
+        f:row { -- the root node
+            fill_horizonal = 1,
+            spacing = f:label_spacing(),
+            -- add controls
+            f:checkbox {
+                title = "Enable", -- label text
+                value = LrView.bind( "isChecked" ) -- bind button state to data key
+                },
+            f:slider {
+                value = LrView.bind( "sliderOne" ),
+                min = 0,
+                max = 100,
+                width = LrView.share( "slider_width" )
             },
-        --f:edit_field {
-        --    value = "Test Hallo",
-        --    enabled = LrView.bind( "isChecked" ) -- bind state to same key
-        --    },
-        --}
-        f:slider {
-            value = LrView.bind( "sliderOne" ),
-            min = 0,
-            max = 100,
-            width = LrView.share( "slider_width" )
+    },
+        f:row { -- the root node
+                fill_horizonal = 1,
+                spacing = f:label_spacing(),
+                -- add controls
+                f:checkbox {
+                    title = "Enable", -- label text
+                    value = LrView.bind( "isChecked" ) -- bind button state to data key
+                    },
+                f:slider {
+                    value = LrView.bind( "sliderTwo" ),
+                    min = 0,
+                    max = 100,
+                    width = LrView.share( "slider_width" )
+                },
+        },
+        f:row { -- the root node
+                fill_horizonal = 1,
+                spacing = f:label_spacing(),
+                -- add controls
+                f:checkbox {
+                    title = "Enable", -- label text
+                    value = LrView.bind( "isChecked" ) -- bind button state to data key
+                    },
+                f:slider {
+                    value = LrView.bind( "sliderThree" ),
+                    min = 0,
+                    max = 100,
+                    width = LrView.share( "slider_width" )
+                },
+        },
+        f:row { -- the root node
+                fill_horizonal = 1,
+                spacing = f:label_spacing(),
+                -- add controls
+                f:checkbox {
+                    title = "Enable", -- label text
+                    value = LrView.bind( "isChecked" ) -- bind button state to data key
+                    },
+                f:slider {
+                    value = LrView.bind( "sliderFour" ),
+                    min = 0,
+                    max = 100,
+                    width = LrView.share( "slider_width" )
+                },
+        },
+        f:row { -- the root node
+                fill_horizonal = 1,
+                spacing = f:label_spacing(),
+                -- add controls
+                f:checkbox {
+                    title = "Enable", -- label text
+                    value = LrView.bind( "isChecked" ) -- bind button state to data key
+                    },
+                f:slider {
+                    value = LrView.bind( "sliderFive" ),
+                    min = 0,
+                    max = 100,
+                    width = LrView.share( "slider_width" )
+                },
         },
     }
     
             local result = LrDialogs.presentModalDialog(
                 {
-                title = "Custom Dialog",
-                contents = c, -- the view hierarchy we defined
+                title = "Dummy Plugin",
+                contents = contents, -- the view hierarchy we defined
                 }
             )                 
     end)
