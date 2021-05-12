9cf77be7d7754e40855a573fff5a0feee713191c
ReginaBarsukov <regina_bars@outlook.de>
2021-05-12 19:55:08 +0200

    Version with a text field
    Updated version with a text field and an update-button to update the plug-in + a version with the sliders.
    The version with the text field is commented. If you want to run the plug-in with the text field, please uncomment and comment the code with the sliders.
    
    Co-Authored-By: aanniiee1209 <69016207+aanniiee1209@users.noreply.github.com>

--- Dummy plugin/dummyplugin.lrdevplugin/LibraryMenuItem.lua
+++ Dummy plugin/dummyplugin.lrdevplugin/LibraryMenuItem.lua
@@ -3,7 +3,70 @@ local LrBinding = import 'LrBinding'
 local LrDialogs = import 'LrDialogs'
 local LrView = import 'LrView'
 local LrColor = import 'LrColor'
-
+------------------------------------------------------------------------------------------------------
+--Version mit "Update Text"--
+------------------------------------------------------------------------------------------------------
+--[[ MyDummyPluginLibraryItem = {}
+function MyDummyPluginLibraryItem.showCustomDialogWithObserver()
+-- body of show-dialog function
+LrFunctionContext.callWithContext( "showCustomDialogWithObserver", function( context )
+    -- body of called function
+    local props = LrBinding.makePropertyTable( context ) -- create bound table
+    props.myObservedString = "This is a string" -- new prop with initial value
+    --props.isChecked = false -- add a property key and initial value
+    -- create view hierarchy
+    local f = LrView.osFactory()
+    local showValue_st = f:static_text { -- create the label control
+    title = props.myObservedString,-- set text to the static property value
+    text_color = LrColor( 1, 0, 0 ) -- set color to a new color object
+    }
+    local updateField = f:edit_field { -- create an edit box
+    value = "Enter some text", -- initial text, not bound to data
+    immediate = true -- update value with every keystroke
+    }
+    local myCalledFunction = function()
+        showValue_st.title = updateField.value -- reflect the value entered in edit box
+        showValue_st.text_color = LrColor( 1, 0, 0 ) -- make the text red
+        end
+    props:addObserver( "myObservedString", myCalledFunction )
+    local c = f:column { --The top-level container, arranges all the rows vertically
+    f:row { -- a group of labels arranged horizontally
+        fill_horizontal = 1, -- the row fills its parent’s width
+        f:static_text { -- add a right-aligned label
+            alignment = "right",
+            width = LrView.share "label_width", -- all get the same width
+            title = "Bound value: "
+        },
+        showValue_st, -- the text box we already defined
+        }, -- end f:row
+    f:row { -- another group, a labeled edit box and button
+        f:static_text {
+            alignment = "right",
+            width = LrView.share "label_width", -- shared with other label
+            title = "New value: "
+        },
+        updateField, -- the edit box we already defined
+        -- add push button
+        f:push_button {
+            title = "Update",
+            action = function() -- when clicked, reset values in other controls
+                showValue_st.text_color = LrColor( 0, 0, 0 ) -- make text black
+                props.myObservedString = updateField.value -- reset data value
+                -- from current entered value
+            end
+            },
+    }, -- end row
+    } -- end column
+    local result = LrDialogs.presentModalDialog {
+        title = "Custom Dialog",
+        contents = c, -- the view hierarchy we defined
+        }
+    end )
+end
+MyDummyPluginLibraryItem.showCustomDialogWithObserver()]]
+------------------------------------------------------------------------------------------------------
+--Version mit den Slider--
+------------------------------------------------------------------------------------------------------
 MyDummyPluginLibraryItem = {}
 function MyDummyPluginLibraryItem.showCustomDialog()
 -- body of show-dialog function
