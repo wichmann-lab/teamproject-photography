76f0c979f6470948d6e37666240b7671b1e50f9a
ReginaBarsukov <regina_bars@outlook.de>
2021-05-16 12:16:15 +0200

    Added File to LrLibraryMenuItem

--- Dummy plugin/dummyplugin.lrdevplugin/Info.lua
+++ Dummy plugin/dummyplugin.lrdevplugin/Info.lua
@@ -1,17 +1,23 @@
 return {
-LrSdkVersion = 9.0,
-LrToolkitIdentifier = "com.adobe.lightroom.sdk.dummyplugin",
-LrPluginName = "Dummy Plugin",
+    LrSdkVersion = 9.0,
+    LrToolkitIdentifier = "com.adobe.lightroom.sdk.dummyplugin",
+    LrPluginName = "Dummy Plugin",
 
 
-LrExportMenuItems = {
-title = "Dummy Plugin Exp", -- The display text for the menu item
-file = "ExportMenuItem.lua", -- The script that runs when the item is selected
-},
+    LrExportMenuItems = {
+        title = "Dummy Plugin Exp", -- The display text for the menu item
+        file = "ExportMenuItem.lua", -- The script that runs when the item is selected
+    },
 
-LrLibraryMenuItems = {
-title = "Dummy Plugin Lib", -- The display text for the menu item
-file = "LibraryMenuItem.lua", -- The script that runs when the item is selected
-},
+    LrLibraryMenuItems = {
+        {
+            title = "Dummy Plugin Lib", -- The display text for the menu item
+            file = "LibraryMenuItem.lua", -- The script that runs when the item is selected
+        },
+        {
+            title="Dummy Plugin with Radio Button", -- The display text for the menu item
+            file="LibraryMenuItemRadioButton",
+        }
+    }
 
 }
