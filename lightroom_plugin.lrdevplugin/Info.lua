32040ef45077f415a84446fbf2fc8069fc1e4ec8
ReginaBarsukov <regina_bars@outlook.de>
2021-05-25 15:37:53 +0200

    Added new file

--- lightroom_plugin.lrdevplugin/Info.lua
+++ lightroom_plugin.lrdevplugin/Info.lua
@@ -3,5 +3,11 @@ return {
     LrToolkitIdentifier = "com.adobe.lightroom.sdk.teamprojectPlugin",
     LrPluginName = "Teamprojekt Plugin",
 
-
+LrLibraryMenuItems={
+        {
+            title= "Teamprojekt Plugin Import",
+            file ="ImportPhotos.lua",
+            enabledWhen = 'photosAvailable',
+        },
+    }
 }
