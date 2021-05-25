c4c0959d6a255f3518e5351acd47f6fbe2e49c8e
ReginaBarsukov <regina_bars@outlook.de>
2021-05-25 12:39:56 +0200

    Update ImportPhotos.lua

--- lightroom_plugin.lrdevplugin/ImportPhotos.lua
+++ lightroom_plugin.lrdevplugin/ImportPhotos.lua
@@ -1,4 +1,17 @@
 local LrCatalog = import 'LrCatalog'
 local LrCollection = import 'LrCollection'
+local LrLogger = import 'LrLogger'
+local LrTasks = import 'LrTasks'
+local LrApplication = import 'LrApplication'
 
-LrCatalog.triggerImportUI(configFile.path)
+local catalog = LrApplication.activeCatalog()
+--LrCatalog.triggerImportUI(configFile.path)
+
+function importSelectedPhotos ()
+    LrTasks.startAsyncTask(function()
+        catalog:withWriteAccessDo("Ein Test", function ()
+            local selectedPhotos = catalog.targetPhoto
+        end)
+    end)
+end
+importSelectedPhotos()
