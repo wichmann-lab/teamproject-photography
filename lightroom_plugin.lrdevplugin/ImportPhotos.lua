5b06936b3e06a65aa4a19b8a5a4ddb8bf3a33678
ReginaBarsukov <regina_bars@outlook.de>
2021-05-25 15:37:39 +0200

    Added the import file to Info.lua
    Added title and a new file

--- lightroom_plugin.lrdevplugin/ImportPhotos.lua
+++ lightroom_plugin.lrdevplugin/ImportPhotos.lua
@@ -1,17 +1,30 @@
-local LrCatalog = import 'LrCatalog'
-local LrCollection = import 'LrCollection'
 local LrLogger = import 'LrLogger'
 local LrTasks = import 'LrTasks'
 local LrApplication = import 'LrApplication'
+local LrDialogs = import 'LrDialogs'
+local LrView = import 'LrView'
+local LrFunctionContext = import 'LrFunctionContext'
+local LrFileUtils = import 'LrFileUtils'
 
-local catalog = LrApplication.activeCatalog()
---LrCatalog.triggerImportUI(configFile.path)
+--Logger for this file 
+importLogger = LrLogger('ImportPhotos')
+importLogger:enable("logfile")
 
-function importSelectedPhotos ()
-    LrTasks.startAsyncTask(function()
-        catalog:withWriteAccessDo("Ein Test", function ()
-            local selectedPhotos = catalog.targetPhoto
-        end)
-    end)
+
+selectedPhotos= {}
+-- function importSelected, selects photos from the catalog and calls the funcion editPhotos()
+function selectedPhotos.importSelected ()
+        local catalog = LrApplication.activeCatalog()
+        local targetPhotos = catalog:getTargetPhotos()
+        if 'ok' == LrDialogs.confirm('Are you sure?', 'Do you want to edit the selected ' .. #(catalog.targetPhotos) .. ' photo(s)?') then
+            for i, photo in ipairs(catalog.targetPhotos) do
+                selectedPhotos.editPhotos(photo)
+            end
+        end
 end
-importSelectedPhotos()
+-- function for editing each photo
+function selectedPhotos.editPhotos(photo)
+    photo:rotateLeft()
+end
+
+selectedPhotos.importSelected()
