112e4ea17e15bb3c42be0e75ca8cfdd7342e860c
aanniiee1209 <donganh.vo@web.de>
2021-05-28 16:52:07 +0200

    Update ImportPhotos.lua

--- lightroom_plugin.lrdevplugin/ImportPhotos.lua
+++ lightroom_plugin.lrdevplugin/ImportPhotos.lua
@@ -11,23 +11,23 @@ local LrFileUtils = import 'LrFileUtils'
 --importLogger:enable("logfile")
 
 
-selectedPhotos= {}
+ImportPhotos= {}
 -- function importSelected, selects photos from the catalog and calls the funcion editPhotos()
-function selectedPhotos.importSelected ()
+function ImportPhotos.importSelected ()
     LrTasks.startAsyncTask( function()
         local catalog = LrApplication.activeCatalog()
         local targetPhotos = catalog:getTargetPhotos()
         if 'ok' == LrDialogs.confirm('Are you sure?', 'Do you want to edit the selected ' .. #(catalog.targetPhotos) .. ' photo(s)?') then
             for i, photo in ipairs(catalog.targetPhotos) do
-                selectedPhotos.editPhotos(photo)
+                ImportPhotos.editPhotos(photo)
             end
         end
     end)
 end
 -- function for editing each photo
-function selectedPhotos.editPhotos(photo)
+function ImportPhotos.editPhotos(photo)
     photo:quickDevelopAdjustImage("Contrast", 100)
     photo:quickDevelopAdjustImage("Highlights", 100)
     photo:quickDevelopAdjustImage("Saturation", 100)
 end
-selectedPhotos.importSelected()
+ImportPhotos.importSelected()
