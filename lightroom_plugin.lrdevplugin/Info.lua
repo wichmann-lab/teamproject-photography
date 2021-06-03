21a80ca5f449982cbbcb530e17c4c56ff40f25f5
aanniiee1209 <donganh.vo@web.de>
2021-06-03 16:51:31 +0200

    Update Info.lua

--- lightroom_plugin.lrdevplugin/Info.lua
+++ lightroom_plugin.lrdevplugin/Info.lua
@@ -1,20 +1,14 @@
 return {
     LrSdkVersion = 9.0,
-    LrToolkitIdentifier = "com.adobe.lightroom.sdk.teamprojectPlugin",
-    LrPluginName = "Teamprojekt Plugin",
+    LrToolkitIdentifier = "com.adobe.lightroom.sdk.theImageIterator",
+    LrPluginName = "The Image Iterator",
 
 LrLibraryMenuItems={
 
     {
         title= "Teamprojekt Plugin UI",
         file ="LibraryMenuItemPluginUI.lua",
-        --enabledWhen = 'photosAvailable',
     },
-       --[[ {
-            title= "Teamprojekt Plugin Import",
-            file ="ImportPhotos.lua",
-            enabledWhen = 'photosAvailable',
-        },]]
 
     }
 }
