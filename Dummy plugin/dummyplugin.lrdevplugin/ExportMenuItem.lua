87645ea07d1a5ff985de735f76cebf043f7a2653
aanniiee1209 <donganh.vo@web.de>
2021-07-26 21:16:34 +0200

    Clean up repository

--- Dummy plugin/dummyplugin.lrdevplugin/ExportMenuItem.lua
+++
@@ -1,8 +0,0 @@
-local LrDialogs = import 'LrDialogs'
-
-MyDummyPluginExportItem = {}
-    function MyDummyPluginExportItem.showModalDialog()
-    LrDialogs.message("ExportMenuItem Selected", "Dummy Plugin", "info")
-end
-
-MyDummyPluginExportItem.showModalDialog()
